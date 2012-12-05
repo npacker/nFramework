af734ba4bdc222127acc7b89afe6a63331e4b4b0
Nigel <Nigel@10.203.6.237>
2012-12-04 22:26:06 -0700

    Updated autoloader.

--- RecipeApp/library/autoload.php
+++ RecipeApp/library/autoload.php
@@ -5,8 +5,10 @@ function __include_file($filename) {
 
 	if (file_exists(ROOT . DS . 'library' . DS . $filename . '.class.php')) {
 		require_once ROOT . DS . 'library' . DS . $filename . '.class.php';
+	} else if (file_exists(ROOT . DS . 'application' . DS . $filename . '.class.php')) {
+		require_once ROOT . DS . 'application' . DS . $filename . '.class.php';
 	} else if (file_exists(ROOT . DS . 'application' . DS . 'controllers' . DS . $filename . '.class.php')) {
-		require_once DS . 'application' . DS . 'controllers' . DS . $filename . '.class.php';
+		require_once ROOT . DS . 'application' . DS . 'controllers' . DS . $filename . '.class.php';
 	} else if (file_exists(ROOT . DS . 'application' . DS . 'models' . DS . $filename . '.class.php')) {
 		require_once ROOT . DS . 'application' . DS . 'models' . DS . $filename . '.class.php';
 	} else {
