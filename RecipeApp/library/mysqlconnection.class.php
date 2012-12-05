f7ec1874e3ef4b90b6df17ff448807f3d89000e6
Nigel <Nigel@10.203.6.237>
2012-12-05 16:03:37 -0700

    Restructuring mysqlconnection class.

--- RecipeApp/library/mysqlconnection.class.php
+++ RecipeApp/library/mysqlconnection.class.php
@@ -3,13 +3,16 @@
 class MySqlConnection {
 
 	public static function getConnection() {
+		$dsn = 'mysql:host=' . DB_HOSTNAME . ';dbname=' . DB_DATABASE;
+
 		try {
-			$dsn = 'mysql:host=' . DB_HOSTNAME . ';dbname=' . DB_DATABASE;
 			$connection = new PDO($dsn, DB_USERNAME, DB_PASSWORD);
-			$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		} catch (PDOException $e) {
 			echo $e->getMessage();
+			exit();
 		}
+
+		$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 		return $connection;
 	}
