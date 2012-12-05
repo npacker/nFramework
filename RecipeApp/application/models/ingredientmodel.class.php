b57c166724a9ccd85d87b1fe40e301fd178708ac
Nigel <Nigel@10.203.6.237>
2012-12-05 10:17:25 -0700

    Getting ingredients for the requested recipe.

--- RecipeApp/application/models/ingredientmodel.class.php
+++ RecipeApp/application/models/ingredientmodel.class.php
@@ -12,6 +12,21 @@ class IngredientModel extends Model {
 		return $result;
 	}
 
+	public function viewAll($recipeId) {
+		$query = 'SELECT name, quantity FROM ingredients WHERE recipe_id = :recipe_id';
+		$statement = $this->connection->prepare($query);
+		$statement->bindParam(':recipe_id', $recipeId);
+		$statement->execute();
+		$statement->setFetchMode(PDO::FETCH_CLASS, 'Ingredient');
+		$ingredients = array();
+
+		while ($result = $statement->fetch()) {
+			array_push($ingredients, $result);
+		}
+
+		return $ingredients;
+	}
+
 	public function create($id) {}
 
 	public function update($id) {}
--- RecipeApp/application/models/recipemodel.class.php
+++ RecipeApp/application/models/recipemodel.class.php
@@ -3,14 +3,19 @@
 class RecipeModel extends Model {
 
 	public function view($id) {
+
+
+
+	}
+
+	protected function getIngredients($id, &$recipe) {
 		try {
 			$ingredientModel = new ingredientModel();
-			$ingredientController = new IngredientController($ingredientModel, __FUNCTION__, $id);
 		} catch (Exception $e) {
 			echo $e->getMessage();
 		}
 
-
+		return $ingredientModel->viewAll($id);
 	}
 
 }
