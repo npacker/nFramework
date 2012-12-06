bceb8a33d288ecd7356a0bbe214c3c9c132f1683
Nigel <Nigel@10.201.135.84>
2012-12-06 12:11:04 -0700

    install.php

--- RecipeApp/install.php
+++ RecipeApp/install.php
@@ -6,18 +6,10 @@ define('ROOT', getcwd());
 require_once (ROOT . DS . 'library' . DS . 'config.php');
 require_once (ROOT . DS . 'library' . DS . 'autoload.php');
 
-$query = 'DROP TABLE IF EXISTS recipes;
-					CREATE TABLE recipes
+$query = 'CREATE TABLE recipes
 					(
 						id INT PRIMARY_KEY AUTO_INCREMENT NOT NULL,
 						name VARCHAR(255) NOT NULL
-					);
-					DROP TABLE IF EXISTS ingredients;
-					CREATE TABLE ingredients
-					(
-						id INT PRIMARY_KEY AUTO_INCREMENT NOT NULL,
-						name VARCHAR(255) NOT NULL,
-						quantity INT NOT NULL
 					);';
 
 $connection = MySqlConnection::getConnection();
@@ -35,7 +27,3 @@ try {
 	echo $e->getMessage();
 	exit();
 }
-
-$query = 'SHOW TABLES;';
-$statement = $connection->prepare($query);
-$statement->execute();
