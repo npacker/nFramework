80bea623c87741737c8722afaacab82b611324a6
Nigel <Nigel@172.23.196.32>
2012-12-14 13:38:40 -0700

    More test data.

--- RecipeApp/install.php
+++ RecipeApp/install.php
@@ -26,7 +26,7 @@ $query = 'DROP TABLE IF EXISTS ingredients;
 						name VARCHAR(255) NOT NULL
 					);
 					INSERT INTO recipes (name) VALUES (\'Test Recipes 1\');
-		';
+					INSERT INTO ingredients (name, quantity, recipe_id)  VALUES (\'Test Ingredient 1\', 1, 1);';
 
 $connection = MySqlConnection::getConnection();
 $statement = $connection->prepare($query);
