44d9c22fe910412adc027f02ac48ca2a5cc93c07
Nigel <Nigel@10.203.6.237>
2012-12-18 22:33:53 -0700

    Signed-off-by: Nigel <Nigel@10.203.6.237>

--- RecipeApp/library/bootstrap.php
+++ RecipeApp/library/bootstrap.php
@@ -2,7 +2,6 @@
 
 require_once (ROOT . DS . 'library' . DS . 'config.php');
 require_once (ROOT . DS . 'library' . DS . 'autoload.php');
-require_once (ROOT . DS . 'library' . DS . 'router.php');
 
 function pathInit() {
 
