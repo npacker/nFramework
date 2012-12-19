0ce188516901aab14e34f591550c605be2187323
Nigel <Nigel@10.203.6.237>
2012-12-18 22:33:41 -0700

    Signed-off-by: Nigel <Nigel@10.203.6.237>

--- RecipeApp/library/bootstrap.php
+++ RecipeApp/library/bootstrap.php
@@ -4,6 +4,44 @@ require_once (ROOT . DS . 'library' . DS . 'config.php');
 require_once (ROOT . DS . 'library' . DS . 'autoload.php');
 require_once (ROOT . DS . 'library' . DS . 'router.php');
 
+function pathInit() {
+
+	echo 'Called ' . __FUNCTION__ . '<br />';
+
+	$uri = $_SERVER['REQUEST_URI'];
+	$params = explode('/', ltrim($uri, '/'));
+	$name = array_shift($params);
+	$action = array_shift($params);
+	$id = array_shift($params);
+
+	if (empty($name) || empty($action) || empty($id)) returnFrontPage();
+	else routeRequest($name, $action, $id);
+}
+
+function routeRequest($name, $action, $id) {
+
+	echo 'Called ' . __FUNCTION__ . '<br />';
+
+	$modelName = $name . 'Model';
+
+	try {
+		$model = new $modelName();
+		$controller = new Controller($model, $action, $id);
+	} catch (Exception $e) {
+		echo $e->getMessage();
+	}
+}
+
+function returnFrontPage() {
+
+	echo 'Called ' . __FUNCTION__ . '<br />';
+
+	$name = 'cookbook';
+	$action = 'view';
+	$id = 1;
+	routeRequest($name, $action, $id);
+}
+
 function bootstrapFull() {
 	pathInit();
 }
