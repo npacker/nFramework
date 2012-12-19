dd13be7499e53ceac701abd91c64f0dea08472d6
Nigel <Nigel@10.203.6.237>
2012-12-18 22:35:04 -0700

    Signed-off-by: Nigel <Nigel@10.203.6.237>

--- RecipeApp/library/bootstrap.php
+++ RecipeApp/library/bootstrap.php
@@ -3,6 +3,18 @@
 require_once (ROOT . DS . 'library' . DS . 'config.php');
 require_once (ROOT . DS . 'library' . DS . 'autoload.php');
 
+function __include_file($filename) {
+	$filename = strtolower($filename);
+
+	if (file_exists(ROOT . DS . 'library' . DS . $filename . '.class.php')) {
+		require_once ROOT . DS . 'library' . DS . $filename . '.class.php';
+	} else if (file_exists(ROOT . DS . 'include' . DS . $filename . '.class.php')) {
+		require_once ROOT . DS . 'include' . DS . $filename . '.class.php';
+	} else {
+		throw new Exception("Could not load $filename.");
+	}
+}
+
 function pathInit() {
 
 	echo 'Called ' . __FUNCTION__ . '<br />';
@@ -42,6 +54,7 @@ function returnFrontPage() {
 }
 
 function bootstrapFull() {
+	spl_autoload_register('__include_file');
 	pathInit();
 }
 
