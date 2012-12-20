6cac8732112e74dea22dbf222a341376572e1f89
Nigel <Nigel@10.203.6.237>
2012-12-19 22:39:00 -0700

    Signed-off-by: Nigel <Nigel@10.203.6.237>

--- RecipeApp/include/recipelist.class.php
+++ RecipeApp/include/recipelist.class.php
@@ -5,10 +5,16 @@ Class RecipeList extends Node {
 	protected $recipeList = array();
 
 	public function __construct(Array $recipeList=null) {
+
+		echo 'Called ' . __METHOD__ . "<br />";
+
 		if (isset($recipeList)) $this->recipeList = $recipeList;
 	}
 
 	public function addRecipe(Recipe $recipe) {
+
+		echo 'Called ' . __METHOD__ . "<br />";
+
 		array_push($this->recipeList, $recipe);
 	}
 
