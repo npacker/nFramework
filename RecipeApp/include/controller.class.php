4b4ab870ca1109735f3046a5926d651b17b44660
Nigel <Nigel@10.203.6.237>
2012-12-20 20:49:31 -0700

    Change terminology related to passing data to view.
    Signed-off-by: Nigel <Nigel@10.203.6.237>

--- RecipeApp/include/controller.class.php
+++ RecipeApp/include/controller.class.php
@@ -2,28 +2,28 @@
 
 class Controller {
 
-	protected $template;
+	protected $view;
 
 	public function __construct(Model $model, $action, $id) {
 
 		echo 'Called ' . __METHOD__ . "<br />";
 
-		// This is a temporary implementation of templates.
-		$this->template = new Template();
+		// This is a temporary implementation of the view layer.
+		$this->view = new View();
 
 		switch ($action) {
 			case 'delete':
-				$this->setTemplateVars($model->delete($id));
+				$this->prepare($model->delete($id));
 				break;
 			case 'update':
-				if (!empty($_POST)) $this->setTemplateVars($model->update($id, $_POST));
+				if (!empty($_POST)) $this->prepare($model->update($id, $_POST));
 				break;
 			case 'create':
-				if (!empty($_POST)) $this->setTemplateVars($model->create($_POST));
+				if (!empty($_POST)) $this->prepare($model->create($_POST));
 				break;
 			case 'view':
-				if (isset($id)) $this->setTemplateVars($model->view($id));
-				else $this->setTemplateVars($model->all());
+				if (isset($id)) $this->prepare($model->view($id));
+				else $this->prepare($model->all());
 				break;
 			default:
 				Throw new Exception('The requested action is undefined.');
@@ -35,10 +35,10 @@ class Controller {
 
 		echo 'Called ' . __METHOD__ . "<br />";
 
-		$this->template->render();
+		$this->view->render();
 	}
 
-	protected function setTemplateVars(Type $type) {
+	protected function prepare(Type $type) {
 
 		echo 'Called ' . __METHOD__ . "<br />";
 
@@ -53,7 +53,7 @@ class Controller {
 
 		echo 'Called ' . __METHOD__ . "<br />";
 
-		$this->template->set($key, $value);
+		$this->view->set($key, $value);
 	}
 
 }
