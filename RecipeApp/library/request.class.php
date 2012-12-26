970876f3515d748bbed47f7f14386953ff19b75c
Nigel <Nigel@192.168.1.103>
2012-12-25 20:44:40 -0800

    Signed-off-by: Nigel <Nigel@192.168.1.103>

--- RecipeApp/library/request.class.php
+++ RecipeApp/library/request.class.php
@@ -7,12 +7,14 @@ class Request {
 	final private function __clone() {}
 
 	public static function get($key, $default=null) {
+		echo 'Called ' . __METHOD__ . "<br />";
 		$value = $default;
 		if (array_key_exists($key, $_GET)) $value = $_GET[$key];
 		return $value;
 	}
 
 	public static function post($key, $default=null) {
+		echo 'Called ' . __METHOD__ . "<br />";
 		$value = $default;
 		if (array_key_exists($key, $_POST)) $value = $_POST[$key];
 		return $value;
