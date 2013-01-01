8a6581128ae3d7bd38d9a025147a3c8ebe384235
Nigel <Nigel@192.168.1.103>
2013-01-01 15:09:29 -0800

    Signed-off-by: Nigel <Nigel@192.168.1.103>

--- RecipeApp/test.php
+++ RecipeApp/test.php
@@ -9,18 +9,22 @@ bootstrapInit();
 
 $database = MySqlDatabase::instance(DB_HOSTNAME, DB_DATABASE, DB_USERNAME, DB_PASSWORD);
 $database->connect();
+
 $recipe = $database->query()
 	->from('recipes', array('name'))
 	->fetchClass('Recipe');
 echo $recipe->name;
+
 $result = $database->query()
 	->from('recipes')
 	->fetchBoth();
 print_r($result);
+
 $database->query()
 	->from('recipes')
 	->save(array('name' => 'Test Recipe 2'));
-$result = $database->query()
+
+$database->query()
 	->from('recipes')
-	->fetchBoth();
-print_r($result);
+	->where('name', '=', 'Test Recipe 2')
+	->delete();
