48178f178ad54b41cae682ab45d559e03b29c122
Nigel <Nigel@10.203.4.218>
2013-01-18 04:12:27 -0700

    Removed unnecessary return statements.
    Signed-off-by: Nigel <Nigel@10.203.4.218>

--- RecipeApp/library/bootstrap.php
+++ RecipeApp/library/bootstrap.php
@@ -45,7 +45,6 @@ function route($type, $action, $id) {
 		$controller = new $controllerName();
 	} catch (FileNotFoundException $e) {
 		throw $e;
-		return;
 	}
 
 	if (method_exists($controller, $action)) {
@@ -53,7 +52,6 @@ function route($type, $action, $id) {
 			(empty($id)) ? $controller->$action() : $controller->$action($id);
 		} catch (Exception $e) {
 			throw $e;
-			return;
 		}
 	}	else throw new BadMethodCallException('Action not defined.');
 }
