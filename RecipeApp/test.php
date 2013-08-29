8558d51ed5e12974ab97fcac3e65c996520e2b0f
Nigel <Nigel@192.168.38.1>
2013-08-28 19:15:47 -0700

    Signed-off-by: Nigel <Nigel@192.168.38.1>

--- RecipeApp/test.php
+++ RecipeApp/test.php
@@ -10,44 +10,83 @@ bootstrapInit();
 $database = MySqlDatabase::instance(DB_HOSTNAME, DB_DATABASE, DB_USERNAME, DB_PASSWORD);
 $database->connect();
 
-$recipe = $database->query('recipes', array('name'))
-	->resultClass('Recipe')
-	->fetch();
-
+try {
+	$recipe = $database->query('recipes', array('name'))
+		->resultClass('Recipe')
+		->fetch();
+} catch (Exception $e) {
+	echo $e->getMessage();
+	exit();
+}
 echo "{$recipe->name}<br />";
 
-$result = $database->query('recipes')
-	->resultClass('Recipe');
-
-while ($recipe = $result->fetch()) {
-	echo "{$recipe->name}<br />";
+try {
+	$result = $database->query('recipes')
+		->resultClass('Recipe');
+} catch (Exception $e) {
+	echo $e->getMessage();
+	exit();
 }
 
-$database->query('recipes')
-	->save(array(
-			'name' => 'Test Recipe 3',
-		));
+try {
+	while ($recipe = $result->fetch()) {
+		echo "{$recipe->name}<br />";
+	}
+} catch (Exception $e) {
+	echo $e->getMessage();
+	exit();
+}
 
-$database->query('recipes')
-	->where('name', 'Test Recipe 2')
-	->save(array(
-			'name' => 'Test Recipe 4',
-		));
+try {
+	$database->query('recipes')
+		->save(array(
+				'name' => 'Test Recipe 3',
+			));
+} catch (Exception $e) {
+	echo $e->getMessage();
+	exit();
+}
 
-$result = $database->query('recipes', array('id'))
-	->where('name', 'Test Recipe 3')
-	->resultBoth()
-	->fetch();
+try {
+	$database->query('recipes')
+		->where('name', 'Test Recipe 2')
+		->save(array(
+				'name' => 'Test Recipe 4',
+			));
+} catch (Exception $e) {
+	echo $e->getMessage();
+	exit();
+}
+
+try {
+	$result = $database->query('recipes', array('id'))
+		->where('name', 'Test Recipe 3')
+		->resultBoth()
+		->fetch();
+} catch (Exception $e) {
+	echo $e->getMessage();
+	exit();
+}
 
 $recipe_id = $result['id'];
 
-$database->query('ingredients')
-	->save(array(
-			'name' => 'Eggs',
-			'quantity' => 2,
-			'recipe_id' => $recipe_id
-		));
+try {
+	$database->query('ingredients')
+		->save(array(
+				'name' => 'Eggs',
+				'quantity' => 2,
+				'recipe_id' => $recipe_id
+			));
+} catch (Exception $e) {
+	echo $e->getMessage();
+	exit();
+}
 
-$database->query('recipes')
-	->where('name', 'Test Recipe 3')
-	->delete();
+try {
+	$database->query('recipes')
+		->where('name', 'Test Recipe 3')
+		->delete();
+} catch (Exception $e) {
+	echo $e->getMessage();
+	exit();
+}
