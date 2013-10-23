e2fdb2292c5971eef90c137d84c36b6b73c3705e
Nigel <Nigel@192.168.56.1>
2013-10-22 19:04:32 -0700

    Set where to extend clause instead of query, and implemented some function prototypes.

--- RecipeApp/library/where.class.php
+++ RecipeApp/library/where.class.php
@@ -1,7 +1,25 @@
 <?php
 
-class Where extends Query {
-	
+class Where extends Clause {
 
-	
+	public function where($column, $value, $operator='=') {
+
+	}
+
+	public function andClause() {
+
+	}
+
+	public function orClause() {
+
+	}
+
+	public function andGroup() {
+
+	}
+
+	public function orGroup() {
+
+	}
+
 }
