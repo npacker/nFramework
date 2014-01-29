9048f6694169fdbf93f18657b27cad6cccf1ddd1
Nigel Packer <packene@gmail.com>
2014-01-28 21:04:34 -0800

    Implementation of HttpException getting exception message text from an HttpError object passed in as an argument to the constructor. The parent constructor is then passed the message as usual.

--- RecipeApp/library/httpexception.class.php
+++ RecipeApp/library/httpexception.class.php
@@ -2,6 +2,9 @@
 
 class HttpException extends Exception {
 
-
+	public function __construct(HttpError $error) {
+		$message = "Error {$error->getCode()}. The requested page {$error->getRequestUrl()} could not be loaded."';
+		parent::__construct($message);
+	}
 
 }
