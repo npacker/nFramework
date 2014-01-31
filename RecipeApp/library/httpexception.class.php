82d19dae682bfae036ea051c3bc04378e19977ea
Nigel Packer <packene@gmail.com>
2014-01-30 20:06:59 -0800

    Removed erroneous character.

--- RecipeApp/library/httpexception.class.php
+++ RecipeApp/library/httpexception.class.php
@@ -3,7 +3,7 @@
 class HttpException extends Exception {
 
 	public function __construct(HttpError $error) {
-		$message = "Error {$error->getCode()}. The requested page {$error->getRequestUrl()} could not be loaded."';
+		$message = "Error {$error->getCode()}. The requested page {$error->getRequestUrl()} could not be loaded.";
 		parent::__construct($message);
 	}
 
