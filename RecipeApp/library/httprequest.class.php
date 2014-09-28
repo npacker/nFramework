1ae286114edc099a4721d824fa44dc587ab159bd
Nigel Packer <packene@gmail.com>
2014-09-28 14:58:02 -0700

    Added check to detect empty query url.

--- RecipeApp/library/httprequest.class.php
+++ RecipeApp/library/httprequest.class.php
@@ -1,31 +1,31 @@
 <?php
 
 class HttpRequest {
+  protected $uri;
+  protected $params;
+  protected $query;
 
-	protected $uri;
-	protected $params;
-	protected $query;
+  public function __construct($uri) {
+    $this->uri = str_replace(base_path(), '', $uri);
+    $this->parseUrl();
+  }
 
-	public function __construct($uri) {
-		$this->uri = str_replace(base_path(), '', $uri);
-		$this->parseUrl();
-	}
+  public function getUri() {
+    return $this->uri;
+  }
 
-	public function getUri() {
-		return $this->uri;
-	}
+  public function getParams() {
+    return $this->params;
+  }
 
-	public function getParams() {
-		return $this->params;
-	}
+  public function getQuery() {
+    return $this->query;
+  }
 
-	public function getQuery() {
-	 return $this->query;
-	}
-
-	protected function parseUrl() {
-		$this->params = parse_path(strstr('?', $this->uri, true));
-		$this->query = parse_query(strstr('?', $this->uri, false));
-	}
-
+  protected function parseUrl() {
+    if (!empty($this->uri)) {
+      $this->params = parse_path(strstr('?', $this->uri, true));
+      $this->query = parse_query(strstr('?', $this->uri, false));
+    }
+  }
 }
