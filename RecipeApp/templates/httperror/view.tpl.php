b991d83e45af008acef16e65715e27b5b50b61d8
Nigel Packer <packene@gmail.com>
2014-10-01 00:22:41 -0700

    Set logic to change class on the httperror view template.

--- RecipeApp/templates/httperror/view.tpl.php
+++ RecipeApp/templates/httperror/view.tpl.php
@@ -1,1 +1,6 @@
-<p class="error warn"><?php echo $message; ?></p>
+<?php if ($code < 500): ?>
+<?php $class = 'warn'; ?>
+<?php else: ?>
+<?php $class = 'critical'; ?>
+<?php endif; ?>
+<p class="error <?php echo $class; ?>"><?php echo $message; ?></p>
