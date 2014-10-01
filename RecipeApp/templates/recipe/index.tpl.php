a2233a9f8c01a464853cf31724f0c925017b85d6
Nigel Packer <packene@gmail.com>
2014-09-30 23:44:32 -0700

    Display recipe index as unordered list.

--- RecipeApp/templates/recipe/index.tpl.php
+++ RecipeApp/templates/recipe/index.tpl.php
@@ -1,3 +1,5 @@
+<ul>
 <?php foreach ($recipes as $recipe): ?>
-  <h2><a href="<?php echo "http://{$base_url}{$base_path}/recipe/view/{$recipe->getId()}"; ?>"><?php echo $recipe->getTitle(); ?></a></h2>
+  <li><a href="<?php echo "http://{$base_url}{$base_path}/recipe/view/{$recipe->getId()}"; ?>"><?php echo $recipe->getTitle(); ?></a></li>
 <?php endforeach; ?>
+</ul>
