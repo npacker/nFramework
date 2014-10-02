06eef7a9b210970f9148ca2279d26ede375647d0
Nigel Packer <packene@gmail.com>
2014-10-01 23:00:42 -0700

    Moved SQL to variable for readability and formatting.

--- RecipeApp/includes/recipemodel.class.php
+++ RecipeApp/includes/recipemodel.class.php
@@ -3,8 +3,12 @@
 class RecipeModel extends Model {
 
 	public function find($id) {
+	  $sql = 'SELECT title
+              FROM recipes
+              WHERE id = ?';
+
 	  $this->database->connect();
-	  $result = $this->database->query('SELECT title FROM recipes WHERE id = ?')
+	  $result = $this->database->query($sql)
 	             ->execute(array($id))
 	             ->fetch();
 	  $this->database->close();
@@ -13,8 +17,13 @@ class RecipeModel extends Model {
 	}
 
 	public function all() {
+	  $sql = "SELECT recipes.id, recipes.title AS title, GROUP_CONCAT(ingredients.title SEPARATOR ', ') AS ingredients
+            FROM recipes LEFT JOIN ingredients
+            ON recipes.id = ingredients.recipe_id
+            GROUP BY recipes.title";
+
     $this->database->connect();
-    $result = $this->database->query('SELECT id, title FROM recipes')
+    $result = $this->database->query($sql)
                 ->execute()
                 ->fetchAll();
     $this->database->close();
