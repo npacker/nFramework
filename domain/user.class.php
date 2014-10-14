b8639417f862a729b62d8d58c75561e07d17f9af
Nigel Packer <packene@gmail.com>
2014-10-13 19:02:16 -0700

    Use singular table names.

--- domain/user.class.php
+++ domain/user.class.php
@@ -4,7 +4,7 @@ class User extends DataMapper {
 
   public function find($id) {
     $sql = 'SELECT id, username, password
-            FROM users';
+            FROM user';
 
     if (is_numeric($id)) {
       $sql .= ' WHERE id = ?';
