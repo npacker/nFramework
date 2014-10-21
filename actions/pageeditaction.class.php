537406e7c9827e55735fe3a9a9ffa4a33974b387
Nigel Packer <packene@gmail.com>
2014-10-21 09:37:21 -0700

    Changed variable name from model to mapper.

--- actions/pageeditaction.class.php
+++ actions/pageeditaction.class.php
@@ -3,7 +3,7 @@
 class PageEditAction extends Action {
 
   public function execute(ActionContext $context) {
-    $model = new PageMapper();
+    $mapper = new PageMapper();
     $page = new Page();
     $id = $context->get('path_argument');
     $title = $context->get('title');
@@ -13,9 +13,9 @@ class PageEditAction extends Action {
     if (isset($title) && isset($content)) {
       $page->setTitle($title);
       $page->setContent($content);
-      $model->update($page);
+      $mapper->update($page);
     } else {
-      $model->find($page);
+      $mapper->find($page);
     }
 
     if (!$page->getTitle()) {
@@ -26,7 +26,7 @@ class PageEditAction extends Action {
     $variables['action'] = 'http://' . base_url() . base_path() . '/page/edit/' . $id;
 
     $template = new Template('page/edit', $variables);
-    $template->addScript(array('jquery','ckeditor/ckeditor','editor'));
+    $template->addScript(array('jquery', 'ckeditor/ckeditor', 'editor'));
 
     $data['title'] = "Editing page <em>{$page->getTitle()}</em>";
     $data['content'] = $template;
