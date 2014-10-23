ab4607a9080374fba0b83c186dc3bb1fc951c6d0
Nigel Packer <packene@gmail.com>
2014-10-23 00:18:25 -0700

    Changed name of dispatcher to application an changed method name to reflect the change.

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ define('ROOT', getcwd());
 require_once ROOT . DS . 'system' . DS . 'bootstrap.php';
 
 bootstrap();
-$dispatcher = new Dispatcher();
-$dispatcher->forward(
+$app = new Application();
+$app->serve(
   new Request($_GET, $_POST, $_SERVER),
   new Response()
 );
