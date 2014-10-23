0123ac763fc398271ed03bab7024fc351005c42a
Nigel Packer <packene@gmail.com>
2014-10-23 00:21:17 -0700

    Created skeleton componentbuilder class.

---
+++ system/componentbuilder.class.php
@@ -0,0 +1,19 @@
+<?php
+
+class ComponentBuilder {
+
+  public function build($entity, $action) {
+
+
+  }
+
+
+  private function buildAction($action) {
+
+  }
+
+  private function buildView($action) {
+
+  }
+
+}
