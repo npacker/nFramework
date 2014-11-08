9ef92439180a72b2903f316565ba9f69e9901b3e
Nigel Packer <npacker@users.noreply.github.com>
2014-11-08 00:38:58 -0800

    Added new functions and improved old ones.

--- nFramework/common.php
+++ nFramework/common.php
@@ -1,21 +1,35 @@
 <?php
 
 function base_path() {
-  static $basePath;
+  static $base_path;
 
-  if (!isset($basePath)) {
-    $filePath = $_SERVER['PHP_SELF'];
-    $documentRoot = realpath($_SERVER['DOCUMENT_ROOT']);
+  if (!isset($base_path)) {
+    $file_path = $_SERVER['SCRIPT_NAME'];
+    $document_root = realpath($_SERVER['DOCUMENT_ROOT']);
 
-    $basePath = str_replace($documentRoot, '', $filePath);
-    $basePath = explode('/', $basePath);
-    array_pop($basePath);
-    $basePath = implode('/', $basePath);
+    $base_path = str_replace($document_root, '', $file_path);
+    $base_path = explode('/', $base_path);
+    array_pop($base_path);
+    $base_path = implode('/', $base_path);
   }
 
-  return $basePath;
+  return $base_path;
 }
 
 function base_url() {
-  return $_SERVER['HTTP_HOST'];
+  static $base_url;
+  
+  if (!isset($base_url)) {
+    $base_url = protocol() . '://' . $_SERVER['HTTP_HOST'];
+  }
+  
+  return $base_url;
+}
+
+function is_secure() {
+  return (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' || $_SERVER['PORT'] == 443)
+}
+
+function protocol() {
+  return (is_secure()) ? 'https' : 'http';
 }
