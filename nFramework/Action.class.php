190acfa27bcbd3eaccd468eb42886f3d2902df33
Nigel Packer <packene@gmail.com>
2014-11-08 13:02:07 -0800

    Inject application into Actions

--- nFramework/Action.class.php
+++ nFramework/Action.class.php
@@ -4,6 +4,12 @@ namespace nFramework;
 
 abstract class Action {
 
+  private $application;
+
+  public function __construct(Application $application) {
+    $this->application = $application;
+  }
+
   abstract public function execute(Context $context);
 
 }
