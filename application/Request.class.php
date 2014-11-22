4fab95a85575b8255d1200ec674434a0d8f1441d
Nigel Packer <packene@gmail.com>
2014-11-22 12:43:50 -0800

    Fixed missing cookie functionality.

--- application/Request.class.php
+++ application/Request.class.php
@@ -4,6 +4,8 @@ namespace nFramework;
 
 class Request {
 
+  protected $cookie;
+
   protected $get;
 
   protected $path;
@@ -14,12 +16,21 @@ class Request {
 
   protected $server;
 
-  public function __construct(array $get, array $post, array $server) {
+  public function __construct(array $cookie, array $get, array $post, array $server) {
+    $this->cookie = $cookie;
     $this->get = $get;
     $this->post = $post;
     $this->server = $server;
     $this->path = new Path($this);
     $this->query = $this->server('QUERY_STRING');
+  }
+
+  public function cookie($key = null) {
+    if ($key) {
+      return $this->get[$key];
+    }
+
+    return $this->cookie;
   }
 
   public function get($key = null) {
