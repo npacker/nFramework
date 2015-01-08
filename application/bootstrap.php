135253492d32be006eca1c13a9e42bed3e4e8f13
Nigel Packer <packene@gmail.com>
2015-01-08 09:38:27 -0800

    Simplified autoloaders

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -8,35 +8,31 @@ use ErrorException;
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', getcwd());
 
-function autoload_action($class) {
+function file_parts($class, &$file, &$namespace) {
   $file = explode('\\', $class);
   $namespace = implode(DS, array_splice($file, 0, 2));
   $file = implode(DS, $file);
-  $file = ROOT . DS . 'packages' . DS . $namespace . DS . 'Controller' . DS . $file . '.class.php';
+}
 
+function require_file($file) {
   if (is_readable($file)) {
     require_once $file;
   }
 }
 
-function autoload_model($class) {
-  $file = explode('\\', $class);
-  $namespace = implode(DS, array_splice($file, 0, 2));
-  $file = implode(DS, $file);
-  $file = ROOT . DS . 'packages' . DS . $namespace . DS . 'Model' . DS . $file . '.class.php';
+function autoload_action($class) {
+  file_parts($class, $file, $namespace);
+  require_file(ROOT . DS . 'packages' . DS . $namespace . DS . 'Controller' . DS . $file . '.class.php');
+}
 
-  if (is_readable($file)) {
-    require_once $file;
-  }
+function autoload_model($class) {
+  file_parts($class, $file, $namespace);
+  require_file(ROOT . DS . 'packages' . DS . $namespace . DS . 'Model' . DS . $file . '.class.php');
 }
 
 function autoload_core($class) {
   $file = str_replace('\\', DS, $class);
-  $file = ROOT . DS . str_replace('nFramework', 'application', $file) . '.class.php';
-
-  if (is_readable($file)) {
-    require_once $file;
-  }
+  require_file(ROOT . DS . str_replace('nFramework', 'application', $file) . '.class.php');
 }
 
 function fatal_error_handler() {
